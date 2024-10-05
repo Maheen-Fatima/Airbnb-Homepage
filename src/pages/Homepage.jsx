5411b056cef72e72f85a03fdbf1cd1b0425b930a
Maheen Fatime <bscs22093@itu.edu.pk>
2024-10-05 21:29:45 +0500

    add Searchbar component to homepag

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,13 +1,21 @@
+// src/pages/Homepage.jsx
 import React from 'react';
 import Navbar from '../components/Navbar';  
+import '../css/Homepage.css';
+import Searchbar from '../components/Searchbar';  
+import homepageImage from '../assets/picture.jpg';  // Adjust this path if needed
 
 function Homepage() {  
   return (
     <div className="homepage">
       <Navbar />
+      <Searchbar />
+
+      <div className="homepage-image-container">
+        <img src={homepageImage} alt="Beautiful Scenery" className="homepage-image" />
+      </div>
     </div>
   );
 }
 
-
-export default Homepage;  
+export default Homepage;
