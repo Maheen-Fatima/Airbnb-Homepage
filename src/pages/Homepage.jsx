6125ca9638ebbf7ca785298a7e10756969ee307f
Maheen Fatime <bscs22093@itu.edu.pk>
2024-10-06 02:00:06 +0500

    add categorie component to homepag

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,16 +1,16 @@
-// src/pages/Homepage.jsx
 import React from 'react';
 import Navbar from '../components/Navbar';  
 import '../css/Homepage.css';
 import Searchbar from '../components/Searchbar';  
-import homepageImage from '../assets/picture.jpg';  // Adjust this path if needed
+import Categories from '../components/Categories'; 
+import homepageImage from '../assets/picture.jpg'; 
 
 function Homepage() {  
   return (
     <div className="homepage">
       <Navbar />
       <Searchbar />
-
+      <Categories />
       <div className="homepage-image-container">
         <img src={homepageImage} alt="Beautiful Scenery" className="homepage-image" />
       </div>
